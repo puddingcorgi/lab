b7e61d168ed936c9e6b3c8fbe38525af7adb7029
puddingcorgi <2561672701@qq.com>
2025-10-29 16:16:14 +0800

    0

---
+++ My project（2）/Assets/TutorialInfo/Scripts/text.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleTextCharge : MonoBehaviour
+{
+    public Text chargeText;
+    private float chargeTimer = 0f;
+
+    void Start()
+    {
+        if (chargeText != null)
+        {
+            chargeText.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        // 鼠标左键蓄力
+        if (Input.GetMouseButton(0))
+        {
+            chargeTimer += Time.deltaTime;
+            float progress = Mathf.Clamp01(chargeTimer / 3f);
+            int percentage = Mathf.RoundToInt(progress * 100);
+
+            if (chargeText != null)
+            {
+                chargeText.gameObject.SetActive(true);
+                chargeText.text = $"{percentage}%";
+            }
+        }
+        else
+        {
+            chargeTimer = 0f;
+            if (chargeText != null)
+            {
+                chargeText.gameObject.SetActive(false);
+            }
+        }
+    }
+}
